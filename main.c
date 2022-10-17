c3afffd708880b4de98f864ec143be3d4a33dd7b
Francis Lahoud <francislahoud@MacBook-Pro-de-Francis.local>
2022-10-16 20:51:47 -0400

    newversion2.0

--- main.c
+++ main.c
@@ -8,18 +8,31 @@
 void	exe(t_vars *vars, char **envp)
 {
 	pid_t	pid;
+	int	i;
 
-	for (int i = 0; vars->cmds[i]; i++) {
+	i = 0;
+	while (vars->cmds[i])
+	{
 		pid = fork();
 		if (pid == 0) {
 			execve(vars->cmds[i][0], vars->cmds[i], envp);
 			write(STDERR_FILENO, "error: cannot execute ", 22);
 			write(STDERR_FILENO, vars->cmds[i][0], ft_strlen(vars->cmds[i][0]));
-			write(STDERR_FILENO, "this\n", 5);
+			write(STDERR_FILENO, "\n", 1);
+			exit(127);
 		}
 		wait(&pid);
+		i++;
 	}
+}
 
+void	set_stops(t_vars *vars) {
+	for (int i = 0; vars->cmds[i]; i++) {
+		for (int j = 0; vars->cmds[i][j]; j++) {
+			if (!strcmp(vars->cmds[i][j], "|") || !strcmp(vars->cmds[i][j], ";"))
+				vars->cmds[i][j] = NULL;
+		}
+	}
 }
 
 void	set_commands(t_vars *vars, char **argv)
@@ -27,9 +40,9 @@ void	set_commands(t_vars *vars, char **argv)
 	int	cmd_list;
 
 	cmd_list = 0;
-	for (int i = 1; argv[i]; i++) {
-		if (i == 1 || !strcmp(argv[i], "|") || !strcmp(argv[i], ";")) {
-			vars->cmds[cmd_list] = &argv[i];
+	for (int i = 0; argv[i]; i++) {
+		if (i == 0 || !strcmp(argv[i], "|") || !strcmp(argv[i], ";")) {
+			vars->cmds[cmd_list] = &argv[++i];
 			if (i == 1 || !strcmp(argv[i - 1], ";")) {
 				vars->position[cmd_list] = FIRST;
 			}
@@ -38,9 +51,18 @@ void	set_commands(t_vars *vars, char **argv)
 			else
 				vars->position[cmd_list] = OTHER;
 			cmd_list++;
-			if (!strcmp(argv[i], "|") || !strcmp(argv[i], ";"))
-				argv[i] = NULL;
 		}
+	}
+	vars->cmds[cmd_list] = NULL;
+}
+
+void	print_commands(t_vars *vars) {
+	for (int i = 0; vars->cmds[i]; i++) {
+		printf("Command %d: ", i + 1);
+		for (int j = 0; vars->cmds[i][j]; j++) {
+			printf("%s ", vars->cmds[i][j]);
+		}
+		printf("\n");
 	}
 }
 
@@ -53,6 +75,8 @@ int main(int argc, char **argv, char **envp)
 	vars.cmds = malloc(sizeof(char **) * count_commands(argv, 0));
 	vars.position = malloc(sizeof(int) * count_commands(argv, 0));
 	set_commands(&vars, argv);
+	set_stops(&vars);
+	//print_commands(&vars);
 	exe(&vars, envp);
 	return (0);
 }
